20bad12affbf0b321741a4633e18ecc8ca54d499
1dami <1dami@hz319583.TartangaLH.eus>
2021-05-17 09:19:17 +0200

    ahora forzando

--- src/PruebaC/PruebaC.java
+++ src/PruebaC/PruebaC.java
@@ -3,7 +3,7 @@ package PruebaC;
 public class PruebaC {
 
 	public static void main(String[] args) {
-		System.out.println("pruebaXCommitPush2");
+		System.out.println("pruebaXCommitPush2e");
 		System.out.println("pruebaXCommitPushConflicto");
 
 	}
