1212b782864c4fda115a9fcd55ffb2caff1a84d8
1dami <1dami@hz319583.TartangaLH.eus>
2021-05-17 09:16:02 +0200

    Prueba Commit error git diferente

--- src/PruebaC/PruebaC.java
+++ src/PruebaC/PruebaC.java
@@ -3,7 +3,8 @@ package PruebaC;
 public class PruebaC {
 
 	public static void main(String[] args) {
-		System.out.println("pruebaXCommitPush");
+		System.out.println("pruebaXCommitPush2");
+		System.out.println("pruebaXCommitPushConflicto");
 
 	}
 
